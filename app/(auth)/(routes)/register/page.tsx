49e9a47366d17b4566c96e1e4c26c78babd0c5f9
jinjo <creationer625@outlook.com>
2023-11-28 18:44:14 +0800

    add clerk auth

--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-const RegisterPage = () => {
-    return (
-        <div>
-            LoginPage
-        </div>
-    )
-}
-
-export default RegisterPage
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,13 +1,11 @@
 import Image from 'next/image'
 import {Button} from "@/components/ui/button";
+import {UserButton} from "@clerk/nextjs";
 
 export default function Home() {
     return (
         <div>
-            <div className="text-3xl font-bold text-indigo-500">Home</div>
-            <Button>
-                Click me
-            </Button>
+            <UserButton afterSignOutUrl='/'/>
         </div>
 
     )
